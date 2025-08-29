15ad5584d950bba26fdcb5f9a70a635c1bdb4ec8
kvoigt <24867209@sun.ac.za>
2025-08-29 23:02:02 +0200

    Finally working basic LCD example

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,113 @@
 #include <Arduino.h>
 // #include "pinDefs.h"
-// #include <TFT_eSPI.h> // Graphics and font library for ILI9341 driver chip
+#include <TFT_eSPI.h> // Graphics and font library for ILI9341 driver chip
+
+int cnt = 0;
+#define TFT_GREY 0x5AEB
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+float sx = 0, sy = 1, mx = 1, my = 0, hx = -1, hy = 0;    // Saved H, M, S x & y multipliers
+float sdeg=0, mdeg=0, hdeg=0;
+uint16_t osx=120, osy=120, omx=120, omy=120, ohx=120, ohy=120;  // Saved H, M, S x & y coords
+uint16_t x0=0, x1=0, yy0=0, yy1=0;
+uint32_t targetTime = 0;                    // for next 1 second timeout
+
+static uint8_t conv2d(const char* p); // Forward declaration needed for IDE 1.6.x
+uint8_t hh=conv2d(__TIME__), mm=conv2d(__TIME__+3), ss=conv2d(__TIME__+6);  // Get H, M, S from compile time
+
+bool initial = 1;
 
 void setup() {
   // UART setup
   Serial.begin(115200);          // initialize USB serial
   while (!Serial) { }            // wait for USB connection
   Serial.println("Hello from FireBeetle C6!");
-  // Builtin LED setup
-  pinMode(15, OUTPUT);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-  vTaskDelay(1000); // Delay to allow time to open Serial Monitor
-  Serial.println("Turning on the LED for 1 second...");
-  digitalWrite(15, HIGH); // Turn the LED on (Note that LOW is the voltage level)
-  vTaskDelay(500);       // Wait for a second
-  Serial.println("Turning off the LED for 1 second...");
-  digitalWrite(15, LOW);  // Turn the LED off by making the voltage HIGH
-  vTaskDelay(500);       // Wait for a second
-  // TFT setup
-  // TFT_eSPI tft = TFT_eSPI(); // Invoke library, pins defined in User_Setup.h
-  // tft.init();
-  // tft.setRotation(1);
-  // tft.fillScreen(TFT_BLACK);
-  // tft.setTextColor(TFT_WHITE, TFT_BLACK); // Note: Adding the background
-  // tft.setTextSize(2);
-  // tft.setCursor(0, 0);
-  // vTaskDelay(1000); // Delay to allow time to see the display update
-  // tft.println("Hello, world!");
+  tft.init();
+  tft.setRotation(0);
+  
+  //tft.fillScreen(TFT_BLACK);
+  //tft.fillScreen(TFT_RED);
+  //tft.fillScreen(TFT_GREEN);
+  //tft.fillScreen(TFT_BLUE);
+  //tft.fillScreen(TFT_BLACK);
+  tft.fillScreen(TFT_GREY);
+  
+  tft.setTextColor(TFT_WHITE, TFT_GREY);  // Adding a background colour erases previous text automatically
+  
+  // Draw clock face
+  tft.fillCircle(120, 120, 118, TFT_GREEN);
+  tft.fillCircle(120, 120, 110, TFT_BLACK);
+
+  // Draw 12 lines
+  for(int i = 0; i<360; i+= 30) {
+    sx = cos((i-90)*0.0174532925);
+    sy = sin((i-90)*0.0174532925);
+    x0 = sx*114+120;
+    yy0 = sy*114+120;
+    x1 = sx*100+120;
+    yy1 = sy*100+120;
+
+    tft.drawLine(x0, yy0, x1, yy1, TFT_GREEN);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello, world!");
-  // FLash led every 2 seconds
-  digitalWrite(15, HIGH); // Turn the LED on (Note that LOW is the voltage level)
-  vTaskDelay(200);       // Wait for a second
-  digitalWrite(15, LOW);  // Turn the LED off by making the voltage HIGH
-  vTaskDelay(1800);       // Wait for a second
+  if (targetTime < millis()) {
+    targetTime += 1000;
+    ss++;              // Advance second
+    if (ss==60) {
+      ss=0;
+      mm++;            // Advance minute
+      if(mm>59) {
+        mm=0;
+        hh++;          // Advance hour
+        if (hh>23) {
+          hh=0;
+        }
+      }
+    }
+
+    // Pre-compute hand degrees, x & y coords for a fast screen update
+    sdeg = ss*6;                  // 0-59 -> 0-354
+    mdeg = mm*6+sdeg*0.01666667;  // 0-59 -> 0-360 - includes seconds
+    hdeg = hh*30+mdeg*0.0833333;  // 0-11 -> 0-360 - includes minutes and seconds
+    hx = cos((hdeg-90)*0.0174532925);    
+    hy = sin((hdeg-90)*0.0174532925);
+    mx = cos((mdeg-90)*0.0174532925);    
+    my = sin((mdeg-90)*0.0174532925);
+    sx = cos((sdeg-90)*0.0174532925);    
+    sy = sin((sdeg-90)*0.0174532925);
+
+    if (ss==0 || initial) {
+      initial = 0;
+      // Erase hour and minute hand positions every minute
+      tft.drawLine(ohx, ohy, 120, 121, TFT_BLACK);
+      ohx = hx*62+121;    
+      ohy = hy*62+121;
+      tft.drawLine(omx, omy, 120, 121, TFT_BLACK);
+      omx = mx*84+120;    
+      omy = my*84+121;
+    }
+
+      // Redraw new hand positions, hour and minute hands not erased here to avoid flicker
+      tft.drawLine(osx, osy, 120, 121, TFT_BLACK);
+      osx = sx*90+121;    
+      osy = sy*90+121;
+      tft.drawLine(osx, osy, 120, 121, TFT_RED);
+      tft.drawLine(ohx, ohy, 120, 121, TFT_WHITE);
+      tft.drawLine(omx, omy, 120, 121, TFT_WHITE);
+      tft.drawLine(osx, osy, 120, 121, TFT_RED);
+
+    tft.fillCircle(120, 121, 3, TFT_RED);
+  }
+}
+
+static uint8_t conv2d(const char* p) {
+  uint8_t v = 0;
+  if ('0' <= *p && *p <= '9')
+    v = *p - '0';
+  return 10 * v + *++p - '0';
 }
